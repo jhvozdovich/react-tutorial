07348ab2d2764fb17dbba489eac8363d34da5944
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-08-29 15:57:15 -0700

    refactor with nested components and props

--- src/index.js
+++ src/index.js
@@ -1,54 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            noble_porpoise
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 1:58pm</span>
-          </div>
-          <div className="text">I did it!</div>
-        </div>
-      </div>
-
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            minister_of_waffles
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 2:30pm</span>
-          </div>
-          <div className="text">Wow!</div>
-        </div>
-      </div>
-
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            thefursausage
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 3:46pm</span>
-          </div>
-          <div className="text">Proud of u!</div>
-        </div>
-      </div>
+      <CommentDetail
+        author="noble_porpoise"
+        dateTimePosted="Yesterday at 1:58pm"
+        content="I did it!"
+      />
+      <CommentDetail
+        author="minister_of_waffles"
+        dateTimePosted="Today at 2:30pm"
+        content="Wow!"
+      />
+      <CommentDetail
+        author="thefursausage"
+        dateTimePosted="Today at 3.46"
+        content="proud of u"
+      />
     </div>
   );
 };
