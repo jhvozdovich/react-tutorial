1ed46ec4638f529246474c58d291d2049eba71dc
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-08-29 12:49:15 -0700

    add JSX vs HTML lesson styling and functionality

--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function getButtonText() {
+  return "Click upon me!";
+}
 // Create react component
 const App = () => {
-  return <div>Hewwo World</div>;
+  const buttonText = "Click Me!";
+  const buttonTextObject = { text: "Click Object!" };
+  const style = { backgroundColor: "purple", color: "white" };
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "green", color: "white" }}>
+        Submit
+      </button>
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+      <button style={{ backgroundColor: "red", color: "white" }}>
+        {getButtonText()}
+      </button>
+      <button style={style}>{buttonTextObject.text}</button>
+    </div>
+  );
 };
 
 // Display component on screen
