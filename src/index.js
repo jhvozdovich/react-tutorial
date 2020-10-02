631bcde3809270bc83c01d10ccc3631aa4181114
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-10-02 15:12:25 -0700

    add loading screen and minor refactors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -13,15 +15,18 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage) {
       return <div>Error: {this.state.errorMessage} </div>;
     } else if (this.state.latitude) {
       return <SeasonDisplay latitude={this.state.latitude} />;
     } else {
-      return <div>Loading!</div>;
+      return <Spinner message="Please accept location request" />;
     }
   }
-}
 
+  render() {
+    return <div className="border">{this.renderContent()}</div>;
+  }
+}
 ReactDOM.render(<App />, document.querySelector("#root"));
