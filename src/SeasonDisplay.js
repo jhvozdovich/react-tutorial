8506aeec50f54af6916565c85bb9e43163e96d52
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-10-02 11:51:06 -0700

    refactor ternary logic to config

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season!</div>;
+const seasonConfig = {
+  summer: { text: "Let's go to the beach-each", icon: "sun" },
+  winter: { text: "Brr, I need some socks", icon: "snowflake" },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const month = new Date().getMonth();
+  const season = getSeason(props.latitude, month);
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
